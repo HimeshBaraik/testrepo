2892a433a00a89e5fbb65a79f266491ebe59473c
HimeshBaraik <134226469+HimeshBaraik@users.noreply.github.com>
2023-05-21 23:45:59 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,23 @@
+#introduction and variables
+
+#python uses new lines to complete a command (other languages like c cpp uses a semicolon ;)
+#python relies on indentation to define scope (other languages like c cpp uses curly brackets {})
+
+#variables
+
+#created when you assign a value to it
+#no command for declaring a variable
+
+x = 4
+print(x)
+
+#not need to be declared with any particular type and can even change type after they have been set
+#specifying a particular data type of a variable: casting
+
+y = int(7)
+print(y)
+
+#getting type : type()
+
+print(type(y))
+
